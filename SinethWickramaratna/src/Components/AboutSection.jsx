677055b3fdfe292f31e89487e327211b1e2f491f
Sineth Wickramaratna <sinethwickramaratna@gmail.com>
2026-02-18 09:25:19 +0530

    fix: Update project count in AboutSection to reflect current statistics

--- SinethWickramaratna/src/Components/AboutSection.jsx
+++ SinethWickramaratna/src/Components/AboutSection.jsx
@@ -60,7 +60,7 @@ function AboutSection() {
         </div>
 
         <div className="about-stats">
-          <StatItem icon="🎯" number="0+" label="Projects" delay={0.3} />
+          <StatItem icon="🎯" number="4+" label="Projects" delay={0.3} />
           <StatItem icon="💻" number="20+" label="Technologies" delay={0.4} />
           <StatItem icon="⚡" number="100%" label="Dedication" delay={0.5} />
         </div>
